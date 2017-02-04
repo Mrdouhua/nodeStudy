c63fea8614ef0247a40fbced6ea7c41614204444
Mrdouhua <1377392896@qq.com>
2017-02-04 16:49:42 +0800

    ch07

---
+++ Node与Express开发学习笔记/ch07/README.md
@@ -0,0 +1,9 @@
+﻿## 第7章：Handlebars模板引擎
+模板可以让代码易写，易读，易维护
+clone后安装再npm install/npm i
+#### 主要内容
+* 简单介绍了几种常见的html模板引擎，jade，EJS，JSHTML，重点讲handlebars模板引擎
+* 安装Express提供的Handlebars npm i --save express-handlebars
+* 使用handlebars时需要注意上下文环境
+* 书写组件（页面加载天气）weather.handlebars
+* 实现网上的模板  ==> 未实现
---
+++ Node与Express开发学习笔记/ch07/meadowlark.js
@@ -0,0 +1,120 @@
+var express = require('express');
+var fortune = require('./lib/fortune.js');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+// 设置handlebars视图引擎
+var handlebars = require('express3-handlebars').create({
+    defaultLayout:'main',
+    helpers: {
+        section: function(name, options){
+            if(!this._sections) this._sections = {};
+            this._sections[name] = options.fn(this);
+            return null;
+        }
+    }
+});
+		
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(function(req, res, next){
+	res.locals.showTests = app.get('env') !== 'production' && 
+		req.query.test === '1';
+	next();
+});
+
+// mocked weather data
+function getWeatherData(){
+    return {
+        locations: [
+            {
+                name: 'Portland',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+                weather: 'Overcast',
+                temp: '54.1 F (12.3 C)',
+            },
+            {
+                name: 'Bend',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+                weather: 'Partly Cloudy',
+                temp: '55.0 F (12.8 C)',
+            },
+            {
+                name: 'Manzanita',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+                weather: 'Light Rain',
+                temp: '55.0 F (12.8 C)',
+            },
+        ],
+    };
+}
+
+// middleware to add weather data to context
+app.use(function(req, res, next){
+	if(!res.locals.partials) res.locals.partials = {};
+ 	res.locals.partials.weatherContext = getWeatherData();
+ 	next();
+});
+
+app.get('/', function(req, res){
+	res.render('home');
+});
+
+app.get('/about', function(req, res){
+	res.render('about', { 
+		fortunes: fortune.getFortune(),
+		pageTestScript: '/qa/tests-about.js'
+	 });
+});
+
+app.get('/tours/hood-river', function(req, res){
+	res.render('tours/hood-river');
+});
+app.get('/tours/oregon-coast', function(req, res){
+	res.render('tours/oregon-coast');
+});
+app.get('/tours/request-group-rate', function(req, res){
+	res.render('tours/request-group-rate');
+});
+app.get('/jquery-test', function(req, res){
+	res.render('jquery-test');
+});
+app.get('/nursery-rhyme', function(req, res){
+	res.render('nursery-rhyme');
+});
+app.get('/data/nursery-rhyme', function(req, res){
+	res.json({
+		animal: 'squirrel',
+		bodyPart: 'tail',
+		adjective: 'bushy',
+		noun: 'heck',
+	});
+});
+
+// 定制404
+app.use(function(req, res){
+	res.status(404);
+	res.render('404');
+});
+
+// 500
+app.use(function(req, res){
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
+});
+
+if( app.thing === null ) {
+	console.log('bleat!');
+}
+
+app.listen(app.get('port'), function(){
+	console.log( 'Express started on http://localhost:' + app.get('port') + '; press Ctrl-C.' );
+});
